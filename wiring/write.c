85fc85f2450294bfa0ca5e6f58ab917d28903ea4
Russ Lewis <russl5445@gmail.com>
2018-10-22 11:13:37 -0700

    Working on the very beginning of the wiring-diagram-write code

--- wiring/write.c
+++ wiring/write.c
@@ -7,7 +7,19 @@
 
 int wiring_write(HWC_Wiring *core)
 {
-	fprintf(stderr, "TODO: implement me: %s\n", __func__);
-	return 1;
+	printf("TODO: %s: Add parameters to allow the user to control the output filename.\n",
+	       __func__);
+
+	FILE *fp = fopen("compile.wire", "w");
+	if (fp == NULL)
+	{
+		perror("Could not open the file to write the wiring diagram");
+		return 1;
+	}
+
+assert(0); // TODO
+
+
+	return 0;
 }
 
