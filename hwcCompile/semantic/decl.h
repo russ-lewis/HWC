325f2b8168afa1a37902d82e558aff7de72d0e6b
Russ Lewis <russl5445@gmail.com>
2018-11-05 11:41:22 -0700

    Added a bunch of comments about how declarations are handled

--- hwcCompile/semantic/decl.h
+++ hwcCompile/semantic/decl.h
@@ -6,37 +6,58 @@
 
 
 
-/* this part is used to store the parse tree for the nonterminals
- * 'field', as well as its sub-nonterminals field_decls, etc.  Each
- * of these instances represents 
+/* this struct represents a declaration, as represented in the semantic
+ * phase.  Each of these declarations is inside of a PT_stmt in the parse
+ * tree, but will be extracted, early in the semantic phase, into an array
+ * of structs, stored directly in the PlugType or Part.
  *
- * This nonterminal is used in two places:
- *    - PlugTypes are entirely made up of nothing other than this;
- *      
+ * Since we will never need to know the name of any declaration *AFTER* we
+ * have found it (since we always find it by name), we don't need to store
+ * the name here; it is implicitly encoded as pointers, stored in one or
+ * more NameScope objects.
+ *
+ * The information that this must carry are:
+ *     - The base type of the declared name, which is a pointer to a
+ *       PlugType or Part;
+ *     - The array metadata (since the base types will never be array types)
+ *     - The index where the bits of this declared name begin, inside the
+ *       enclosing PlugType or Part.
  */
-struct PT_decl
-{
-	/* linked list, in reverse order of declaration */
-	PT_decl *prev;
 
-	PT_type *type;
-	char *name;
-	PT_array_decl *arraySuffix;
-};
-
-
-/* TODO */
 typedef struct HWC_Decl HWC_Decl;
 struct HWC_Decl
 {
-	char     *name
-	HWC_Type *type;
+	/* In a Decl that is part of a PlugType, we know for *sure* that
+	 * the base type of the Decl is another PlugType.  In a Part, there
+	 * can be some Decl's that refer to PlugType's (the various plugs of
+	 * the Part), and others that refer to Part's (the various
+	 * subcomponents).
+	 *
+	 * NOTE: We do not need to remember public/private in this struct,
+	 *       because that information is implicitly stored - it simply
+	 *       tells us which NameScope(s) will contain the name for this
+	 *       Decl.
+	 */
+	HWC_PlugType *base_plugType;
+	HWC_Part     *base_part;
 
-	/* this is the size of the array.  If this is NULL, then this *NOT*
-	 * an array, it's a simple instiation of the PlugType
+
+	/* array size.  This is a single expression if the Decl is an array;
+	 * we will validate, near the end of the semantic phase, that it is
+	 * of type INT and that it has a non-negative value.
+	 *
+	 * If this is NULL, then this Decl is not an array.
+	 *
+	 * TODO: support multidimensional arrays in the future.
 	 */
 	HWC_Expr *expr;
+
+
+	/* bit index of this Decl inside its enclosing PlugType or Part */
+	int index;
 };
+
+
 
 int convert PTdeclIntoHWCdecl(PT_Decl *, HWC_Decl *);
 
