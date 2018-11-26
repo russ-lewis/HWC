990e4b79b513ff7b856468869cb81bc7ac4bf68e
Jackson Lindsay <jacksonlindsay@catmail.arizona.edu>
2018-11-26 11:25:06 -0700

    Removed traces of arraySuffix in semantic code, allowed only one-level of TYPE_ARRAY recursion, filled in skeleton of plugtype.

--- hwcCompile/semantic/decl.c
+++ hwcCompile/semantic/decl.c
@@ -12,38 +12,31 @@ void convertPTdeclIntoHWCdecl(PT_decl *input, HWC_Decl *output)
 {
 	output = malloc(sizeof(HWC_Decl));
 
-	assert(0);
-	// Note: We don't set *base_plugType or *base_part yet. Set type so we can check their validity later.
-	//   We could check for and set decls of type "bit" here, but we'll need to check the part/plug name
-	//   later anyhow, so
-	// TODO: SHOULD WE BE CONVERTING PT_TYPE TO A HWC_TYPE INSTEAD?
-
-	// TODO: IF CONVERT->MODE == TYPE_ARRAY, WHAT TO DO WITH CONVERT->LEN? WE ALREADY USE ARRAYSUFFIX BELOW
 	PT_type *convert = input->type;
 	if(convert->mode == TYPE_ARRAY)
+	{
+		convertPTexprIntoHWCexpr(convert->len, output->expr);
 		convert = convert->base;
+	}
+	else
+		output->expr = NULL;
 
+	// TODO: Allow multi-level arrays
+	if(convert->mode == TYPE_ARRAY)
+	{
+		fprintf(stderr, "Multi-level arrays are currently not supported in HWC.\n");
+		assert(0);
+	}
+
+	// Note: We don't set *base_plugType or *base_part yet. Set type so we can check their validity later.
+	//   We could check for and set decls of type "bit" here, but we'll need to check the part/plug name
+	//   later anyhow, so pass on it for now
 	output->type     = convert->mode;
 	output->typeName = convert->ident;
 
 	output->base_plugType = NULL;
 	output->base_part     = NULL;
 
-	// TODO: Below is a copy of the PT_array_decl struct, which is where we get the expr for this decl from
-	//       How should we accout for arraySuffix->prev being valid?
-	/*
-	struct PT_array_decl
-	{
-		PT_array_decl *prev;
-
-		PT_expr *size;
-	};
-	*/
-	if(input->arraySuffix == NULL)
-		output->expr = NULL;
-	else
-		convertPTexprIntoHWCexpr(input->arraySuffix->size, output->expr);
-
 	// Temp dummy value that might be useful later
-	output->index = -1; // TODO: Is this necessary?
+	output->index = -1;
 }
--- hwcCompile/semantic/plugtype.h
+++ hwcCompile/semantic/plugtype.h
@@ -4,10 +4,25 @@
 
 // Maybe just want to reference "plug" struct instead?
 typedef struct plugtype plugtype;
-struct plugtype {
+struct plugtype
+{
+	/* these track the progress of the semantic phase.  The 'completed'
+	 * field tells us which semantic phase is done for this type; calling
+	 * a generator function on this type, for one of these phases, is a
+	 * NOP.  The 'begun' field tells you what phases have begun, which is
+	 * used to detect recursive types.
+	 */
+	int phases_completed;
+	int phases_begun;
 
-	// Temp variable and temp name until I have a better idea of what should go here.
-	char wire;
+	// All names within a plugtype are public
+	HWC_NameScope *publicNames;
+
+	// Only stmts allowed in plugtypes are decls.
+	HWC_Decl *decls;
+	int       decls_len;
+
+	// TODO: Field for sizeof Part
 };
 
 // Presumably put primitive "bit" here
