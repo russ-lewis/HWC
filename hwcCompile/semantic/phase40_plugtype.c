dd78f35ba5ead9e730e2317d9dd6216c7d3acf02
Jackson Lindsay <jacksonlindsay@catmail.arizona.edu>
2019-02-14 13:57:17 -0700

    Polished up phase 30 and 40 ending code slightly. Implemented phase40_plugtype.

--- hwcCompile/semantic/phase40_plugtype.c
+++ hwcCompile/semantic/phase40_plugtype.c
@@ -6,6 +6,27 @@
 
 int semPhase40_plugtype(HWC_PlugType *plugType)
 {
-	assert(0);   // TODO
+	if (plugType->phases_completed >= 40)
+		return 0;
+
+	if (plugType->phases_begun >= 40)
+	{
+		/* report user error */
+		assert(0);
+	}
+
+	if (plugType->phases_completed < 30)
+	{
+		int rc = semPhase30_plugtype(plugType);
+		if (rc != 0)
+			return rc;
+	}
+
+	if (plugType->phases_completed >= 40)
+		return 0;
+
+	// TODO: For now, we skip phase40, out of consideration for time
+	plugType->phases_completed = 40;
+	return 0;
 }
 
