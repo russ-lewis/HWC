b510645aef5fc613a6855a42dd9f74d3c0286462
Dilen Govin <dilengovin@email.arizona.edu>
2019-11-06 08:41:47 -0700

    repository hygiene through git ignore

--- hwcSim_py/wiring/__init__.py
+++ hwcSim_py/wiring/__init__.py
@@ -1,4 +1,4 @@
-# Needed so the wiring directory is treated as a moduel in python
+# Needed so the wiring directory is treated as a module in python
 #
 # Found in the below link:
 # https://stackoverflow.com/questions/456481/cant-get-python-to-import-from-a-different-folder 
