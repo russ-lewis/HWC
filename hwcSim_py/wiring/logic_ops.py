84576493a326f8c0267d7e510a7d488c9a646bc7
Dilen Govin <dilengovin@email.arizona.edu>
2019-11-27 10:24:12 -0700

    Simulator is now driven off of lambdas

--- hwcSim_py/wiring/logic_ops.py
+++ hwcSim_py/wiring/logic_ops.py
@@ -1,3 +1,22 @@
+'''
+case OP_NEQUAL:
+		case OP_LESS:
+		case OP_GREATER:
+		case OP_LESSEQ:
+		case OP_GREATEREQ:
+		case OP_BITAND:
+		case OP_AND:
+		case OP_BITOR:
+		case OP_OR:
+		case OP_XOR:
+		case OP_PLUS:
+		case OP_MINUS:
+		case OP_TIMES:
+		case OP_DIVIDE:
+		case OP_MODULO:
+		case OP_CONCAT:
+'''
+
 class LogicOp(object):
 
     def __init__(self, readers, writers, name):
@@ -71,11 +90,50 @@ class AND(LogicOp):
     def deliver_a(self, val):
         self.val_a = val
 
+        # try to evaluate operation
+        self.evaluate_op()
+
     def deliver_b(self, val):
         self.val_b = val
 
+        # try to evaulate operation
+        self.evaluate_op()
+
+    def evaluate_op(self):
+        if self.val_a == None or self.val_b == None or self.out != None:
+            return
+
+        val = self.val_a + self.val_b
+
+        # True if both 0 or both 1
+        if val == 2:
+            self.out = 1
+        else:
+            self.out = 0
+
+        for reader in self.readers:
+            reader(self.out)
+
+    def get_lambda():
+        return
+
+class OR(LogicOp):
+
+    def __init__(self, readers, writers, name):
+        LogicOp.__init__(self, readers, writers, name)
+
+        self.val_a = None
+        self.val_b = None
+        self.out   = None
+
     def deliver_a(self, val):
         self.val_a = val
+
+        # try to evaluate operation
+        self.evaluate_op()
+
+    def deliver_b(self, val):
+        self.val_b = val
 
         # try to evaluate operation
         self.evaluate_op()
@@ -87,7 +145,7 @@ class AND(LogicOp):
         val = self.val_a + self.val_b
 
         # True if both 0 or both 1
-        if val == 0 or val == 2:
+        if val > 0:
             self.out = 1
         else:
             self.out = 0
